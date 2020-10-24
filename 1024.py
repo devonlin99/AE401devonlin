c149222df03f80d543bef9751fc608a85bfd1f9e
devonlin99 <71509562+devonlin99@users.noreply.github.com>
2020-10-24 11:49:24 +0800

    Add files via upload

---
+++ 1024.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 10:30:20 2020
+
+@author: Devon
+"""
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time
+
+while True:
+
+    x,y,z = mc.player.getTilePos()
+
+    a=mc.getBlock(x,y-1,z-1)
+    b=mc.getBlock(x,y-1,z+1)
+    c=mc.getBlock(x-1,y-1,z)
+    d=mc.getBlock(x+1,y-1,z)
+
+    if a==0 or a == 0 or b == 0 or b == 0 or c == 0 or c ==0:
+        mc.setBlocks(x+1,y-1,z+1,x-1,y-1,z-1,165)
+       
+
+        
+while True:
+    x,y,z = mc.player.getTilePos()
+
+    
+    mc.setBlock(x,y,z,38)
+    time.sleep(0.2)
+
+
+while a < 30:
+    mc.setBlocks(x+20,y-1,z,x-20,y-10,z,19)
+    z=z+5
+    a=a=1
